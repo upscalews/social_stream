cf5a3ea69a9e75f7556afd99132a6901ba4f456c
William Harris <wharris@upscalews.com>
2013-01-30 20:38:57 -0600

    remove sites from search subjects until it works

--- base/app/models/site.rb
+++ base/app/models/site.rb
@@ -3,7 +3,7 @@
 #   Site.config[:host] = 'example.com'
 #   Site.save!
 class Site < ActiveRecord::Base
-  include SocialStream::Models::Subject
+  #include SocialStream::Models::Subject
 
   serialize :config, Hash
 
